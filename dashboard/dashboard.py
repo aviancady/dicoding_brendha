29933254d6cd6dc05c339c183eb9d9c033e40a8a
aviancady <151365072+aviancady@users.noreply.github.com>
2024-10-03 10:31:10 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,54 +1,166 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+%matplotlib inline
+from matplotlib.gridspec import GridSpec
+import matplotlib.lines as mlines
+import matplotlib.image as mpimg
+pd.set_option('display.max_columns', 100)
+import plotly.offline as py
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import os
+import json
+import requests
+import folium
+from folium.plugins import FastMarkerCluster, Fullscreen, MiniMap, HeatMap, HeatMapWithTime, LocateControl
+import datetime as dt
 
-# Ambil kolom data order_count untuk clustering
-data = df_customer_order_count[['order_count']].values
+# Calculate time of mapping process
+import time
+start = time.perf_counter()
 
-# Fungsi untuk menghitung jarak Euclidean (1D)
-def euclidean_distance_1d(point1, point2):
-    return np.abs(point1 - point2)
+# Plot where the costomers are
+import folium
+from folium import Marker
+from folium.plugins import MarkerCluster
 
-# Fungsi untuk menginisialisasi centroid secara acak
-def initialize_centroids_1d(data, k):
-    indices = np.random.choice(len(data), k, replace=False)
-    return data[indices]
+# Make color palette
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.colors import rgb2hex
 
-# Fungsi untuk menetapkan cluster berdasarkan jarak terdekat ke centroid
-def assign_clusters_1d(data, centroids):
-    clusters = []
-    for point in data:
-        distances = [euclidean_distance_1d(point, centroid) for centroid in centroids]
-        clusters.append(np.argmin(distances))
-    return np.array(clusters)
+def make_palette(n_samples, cmap):
+    n_samples = n_samples
+    color_map = plt.get_cmap(cmap)
+    palette = []
+    for c in np.linspace(0.0, 1.0, n_samples*2):
+        color_code = rgb2hex(color_map(c))
+        palette.append(color_code)
+    return palette[n_samples:]
 
-# Fungsi untuk memperbarui posisi centroid
-def update_centroids_1d(data, clusters, k):
-    new_centroids = []
-    for i in range(k):
-        cluster_points = data[clusters == i]
-        new_centroids.append(np.mean(cluster_points))
-    return np.array(new_centroids)
+# Make color producer function
+def color_producer(val):
+    if val <= 45.9:
+        return make_palette(2, "Blues")[0]
+    if val <= 86.9:
+        return make_palette(2, "Blues")[1]
+    if val <= 137.8:
+        return make_palette(1, "Wistia")[0]
+    if val <= 149.9:
+        return make_palette(2, "Reds")[0]
+    else:
+        return make_palette(2, "Reds")[1]
 
-# Fungsi utama K-Means 1D
-def kmeans_clustering_1d(data, k, max_iterations=100):
-    centroids = initialize_centroids_1d(data, k)
-    for i in range(max_iterations):
-        clusters = assign_clusters_1d(data, centroids)
-        new_centroids = update_centroids_1d(data, clusters, k)
-        if np.all(centroids == new_centroids):
-            break
-        centroids = new_centroids
-    return clusters, centroids
+# Add points which is randomly selected(n_samples=1,000) on the map
+# Make a sample data
+import random
+sample_idx = random.sample(purchase_price_df.index.to_list(), k=1000)
+sample_df = purchase_price_df.loc[sample_idx]
 
-# Menggunakan fungsi K-Means pada order_count
-k = 3
-clusters, centroids = kmeans_clustering_1d(data, k)
+# Add bubble map to base map
+from folium import Circle
 
-# Visualisasi hasil clustering
-plt.scatter(df_customer_order_count['customer_unique_id'], df_customer_order_count['order_count'], c=clusters, cmap='viridis', marker='o')
-plt.scatter(range(len(centroids)), centroids, c='red', marker='x', s=100, label='Centroids')
-plt.title("Clustering Customer Order Count (Manual K-Means)")
-plt.xlabel("Customer ID")
-plt.ylabel("Order Count")
-plt.legend()
-plt.show()
+for i in range(0, len(sample_df)):
+    Circle(
+        location=[sample_df.iloc[i]["rep_lat"], sample_df.iloc[i]["rep_long"]],
+        radius=sample_df.iloc[i]["purchase_price"],
+        color=color_producer(sample_df.iloc[i]["purchase_price"]),
+        popup=sample_df.iloc[i]["purchase_price"]
+    ).add_to(m_2)
+
+# Make function for add categorical legend
+def add_categorical_legend(folium_map, title, colors, labels):
+    if len(colors) != len(labels):
+        raise ValueError("colors and labels must have the same length.")
+    color_by_label = dict(zip(labels, colors))
+    legend_categories = ""
+    for label, color in color_by_label.items():
+        legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
+    legend_html = f"""
+    <div id='maplegend' class='maplegend'>
+      <div class='legend-title'>{title}</div>
+      <div class='legend-scale'>
+        <ul class='legend-labels'>
+        {legend_categories}
+        </ul>
+      </div>
+    </div>
+    """
+    script = f"""
+        <script type="text/javascript">
+        var oneTimeExecution = (function() {{
+                    var executed = false;
+                    return function() {{
+                        if (!executed) {{
+                             var checkExist = setInterval(function() {{
+                                       if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
+                                          clearInterval(checkExist);
+                                          executed = true;
+                                       }}
+                                    }}, 100);
+                        }}
+                    }};
+                }})();
+        oneTimeExecution()
+        </script>
+      """
+    css = """
+    <style type='text/css'>
+      .maplegend {
+        z-index:9999;
+        float:right;
+        background-color: rgba(255, 255, 255, 1);
+        border-radius: 5px;
+        border: 2px solid #bbb;
+        padding: 10px;
+        font-size:12px;
+        positon: relative;
+      }
+      .maplegend .legend-title {
+        text-align: left;
+        margin-bottom: 5px;
+        font-weight: bold;
+        font-size: 90%;
+        }
+      .maplegend .legend-scale ul {
+        margin: 0;
+        margin-bottom: 5px;
+        padding: 0;
+        float: left;
+        list-style: none;
+        }
+      .maplegend .legend-scale ul li {
+        font-size: 80%;
+        list-style: none;
+        margin-left: 0;
+        line-height: 18px;
+        margin-bottom: 2px;
+        }
+      .maplegend ul.legend-labels li span {
+        display: block;
+        float: left;
+        height: 16px;
+        width: 30px;
+        margin-right: 5px;
+        margin-left: 0;
+        border: 0px solid #ccc;
+        }
+      .maplegend .legend-source {
+        font-size: 80%;
+        color: #777;
+        clear: both;
+        }
+      .maplegend a {
+        color: #777;
+        }
+    </style>
+    """
+    folium_map.get_root().header.add_child(folium.Element(script + css))
+    return folium_map
